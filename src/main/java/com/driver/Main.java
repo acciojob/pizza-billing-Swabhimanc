1e0181a889d6490d71802e73b2906c83f9246e58
Swabhiman Chaurasiya <swabhimanc@gmail.com>
2023-03-14 18:09:13 +0530

    1st Attempt

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,17 +2,18 @@ package com.driver;
 
 public class Main {
   public static void main(String[] args) {
-    DeluxePizza dp = new DeluxePizza(true);
+    DeluxePizza dp = new DeluxePizza(false);
     System.out.println(dp.getPrice());
     dp.addTakeaway();
     dp.addExtraCheese();
+    dp.addExtraToppings();
     dp.addTakeaway();
     System.out.println(dp.getBill());
 
-    Pizza p = new Pizza(true);
-    p.addExtraCheese();
-    p.addExtraToppings();
-    p.addTakeaway();
-    System.out.println(p.getBill());
+//    Pizza p = new Pizza(true);
+//    p.addExtraCheese();
+//    p.addExtraToppings();
+//    p.addTakeaway();
+//    System.out.println(p.getBill());
   }
 }
--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -6,29 +6,73 @@ public class Pizza {
     private Boolean isVeg;
     private String bill;
 
+    private int vegToppings=0;
+    private int nonVegToppings=0;
+    private int paperbag=0;
+    private int extraCheese=0;
+
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
-        // your code goes here
+
+        if(isVeg==true)
+        {
+            this.price=300;
+        }
+        else
+        {
+            this.price=400;
+        }
     }
 
     public int getPrice(){
         return this.price;
     }
 
-    public void addExtraCheese(){
-        // your code goes here
+    public void addExtraCheese()
+    {
+        this.extraCheese+=80;
     }
 
     public void addExtraToppings(){
-        // your code goes here
+        if(this.isVeg==true)
+        {
+            this.vegToppings+=70;
+        }
+        else
+        {
+            this.nonVegToppings+=120;
+        }
     }
 
     public void addTakeaway(){
-        // your code goes here
+        this.paperbag=20;
     }
 
-    public String getBill(){
-        // your code goes here
+    public String getBill()
+    {
+        String ans="";
+        ans=ans+"Base Price Of The Pizza: "+this.price+"\n";
+
+        if(this.extraCheese!=0)
+        {
+            ans+="Extra Cheese Added: "+this.extraCheese+"\n";
+        }
+        if(this.vegToppings!=0)
+        {
+            ans+="Extra Toppings Added: "+this.vegToppings+"\n";
+        }
+        else if(this.nonVegToppings!=0)
+        {
+            ans+="Extra Toppings Added: "+this.nonVegToppings+"\n";
+        }
+
+        if(this.paperbag!=0)
+        {
+            ans+="Paperbag Added: "+this.paperbag+"\n";
+        }
+        int total=this.price+this.paperbag+this.extraCheese+this.vegToppings+this.nonVegToppings;
+        ans+="Total Price: "+total+"\n";
+        this.bill=ans;
         return this.bill;
     }
 }
